3192b9b452cf49be726d004c5a2f1abd12cd34f5
Andrew Munro <andrew.munro@skybettingandgaming.com>
2017-01-02 13:58:17 +0000

    Adding extra mac addresses. Adding timeout. Hooking into IFTTT

--- DashDetector.js
+++ DashDetector.js
@@ -1,13 +1,17 @@
 let pcap = require('pcap');
 let EventEmitter = require('events').EventEmitter;
 
+const THROTTLE_TIMEOUT = 5000;
+
 class DashDetector extends EventEmitter {
     constructor(addresses, iface) {
         super();
         this.addresses = addresses;
+        this.recentAddresses = [];
 
-        // All dash mac addresses start with ac:63:be
-        this.session = pcap.createSession(iface, 'ether[6:2] == 0xac63 and ether[8:1] == 0xbe');
+        // All dash mac addresses start with ac:63:be or 50:f5:da
+        // If not found, try use filter: 'udp and ( port 67 or port 68 )'
+        this.session = pcap.createSession(iface, '(ether[6:2] == 0xac63 and ether[8:1] == 0xbe) or (ether[6:2] == 0x50f5 and ether[8:1] == 0xda)');
         this.session.on('packet', (packet) => this.handlePacket(packet));
     }
 
@@ -23,6 +27,7 @@ class DashDetector extends EventEmitter {
 
         if(this.shouldEmit(address)) {
             this.emit('press', address);
+            this.throttleAddress(address);
         }
     }
 
@@ -30,7 +35,22 @@ class DashDetector extends EventEmitter {
         return intArray.map(i => i.toString(16)).join(':');
     }
 
+    throttleAddress(address) {
+        this.recentAddresses.push(address);
+        setTimeout(() => {
+            this.recentAddresses.splice(this.recentAddresses.indexOf(address), 1);
+        }, THROTTLE_TIMEOUT)
+    }
+
     shouldEmit(address) {
+        return this.isWhitelisted(address) && !this.pressedRecently(address);
+    }
+
+    pressedRecently(address) {
+        return this.recentAddresses.indexOf(address) != -1
+    }
+
+    isWhitelisted(address) {
         return !this.addresses ||
             this.addresses === address ||
             (this.addresses instanceof Array && this.addresses.indexOf(address) != -1);
--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+let request = require('request');
+
 let DashDetector = require('./DashDetector');
 
 let detector = new DashDetector();
 
-detector.on('press', (mac) => console.log(mac));
+const IFTTT_KEY = '****';
+const EVENT_NAME = 'dash_pressed'
+
+detector.on('press', (mac) => {
+    console.log('Received event from', mac);
+
+    request.post({
+        url: `https://maker.ifttt.com/trigger/${EVENT_NAME}/with/key/${IFTTT_KEY}`
+    }, (error, response, body) => {
+        if(error) {
+            console.error('Error:', error);
+        } else {
+            console.log('Event', EVENT_NAME, 'triggered');
+        }
+    });
+});
+
+console.log('Listening...');
